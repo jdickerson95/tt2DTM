fix: slight scaling updates to set CCG mean=0 var=1 on average

src/tt2dtm/backend/process_results.py
@@ -46,10 +46,16 @@ def aggregate_distributed_results(
     mip_max = mips.max(axis=0)
     mip_argmax = mips.argmax(axis=0)
 
+    # Find which device had the highest MIP for each pixel and index stats accordingly.
+    # Results after 'take_along_axis' have extra dimension at idx 0.
     best_phi = np.take_along_axis(best_phi, mip_argmax[None, ...], axis=0)
     best_theta = np.take_along_axis(best_theta, mip_argmax[None, ...], axis=0)
     best_psi = np.take_along_axis(best_psi, mip_argmax[None, ...], axis=0)
     best_defocus = np.take_along_axis(best_defocus, mip_argmax[None, ...], axis=0)
+    best_phi = best_phi[0]
+    best_theta = best_theta[0]
+    best_psi = best_psi[0]
+    best_defocus = best_defocus[0]
 
     # Sum the sums and squared sums of the cross-correlation values
     correlation_sum = np.stack(
@@ -85,6 +91,35 @@ def aggregate_distributed_results(
     }
 
 
+def correlation_sum_and_squared_sum_to_mean_and_variance(
+    correlation_sum: torch.Tensor,
+    correlation_squared_sum: torch.Tensor,
+    total_correlation_positions: int,
+) -> tuple[torch.Tensor, torch.Tensor]:
+    """Convert the sum and squared sum of the correlation values to mean and variance.
+
+    Parameters
+    ----------
+    correlation_sum : torch.Tensor
+        Sum of the correlation values.
+    correlation_squared_sum : torch.Tensor
+        Sum of the squared correlation values.
+    total_correlation_positions : int
+        Total number cross-correlograms calculated.
+
+    Returns
+    -------
+    tuple[torch.Tensor, torch.Tensor]
+        Tuple containing the mean and variance of the correlation values.
+    """
+    correlation_mean = correlation_sum / total_correlation_positions
+    correlation_variance = correlation_squared_sum / total_correlation_positions
+    correlation_variance -= correlation_mean**2
+    correlation_variance = torch.sqrt(torch.clamp(correlation_variance, min=0))
+
+    return correlation_mean, correlation_variance
+
+
 def scale_mip(
     mip: torch.Tensor,
     mip_scaled: torch.Tensor,
@@ -102,6 +137,10 @@ def scale_mip(
     of Gaussian distributions, so Z-score has to be compared with a generalized extreme
     value distribution (GEV) to determine significance (done elsewhere).
 
+    NOTE: This method also updates the correlation_sum and correlation_squared_sum
+    tensors in-place into the mean and variance, respectively. Likely should reflect
+    conversions in variable names...
+
     Parameters
     ----------
     mip : torch.Tensor
@@ -109,9 +148,9 @@ def scale_mip(
     mip_scaled : torch.Tensor
         Scaled MIP of the correlation values.
     correlation_sum : torch.Tensor
-        Sum of the correlation values.
+        Sum of the correlation values. Updated to mean of the correlation values.
     correlation_squared_sum : torch.Tensor
-        Sum of the squared correlation values.
+        Sum of the squared correlation values. Updated to variance of the correlation.
     total_correlation_positions : int
         Total number cross-correlograms calculated.
 
@@ -120,23 +159,21 @@ def scale_mip(
     tuple[torch.Tensor, torch.Tensor]
         Tuple containing the MIP and scaled MIP
     """
-    num_pixels = torch.tensor(mip.shape[0] * mip.shape[1])
-
-    # Convert sum and squared sum to mean and variance in-place
-    correlation_sum = correlation_sum / total_correlation_positions
-    correlation_squared_sum = correlation_squared_sum / total_correlation_positions
-    correlation_squared_sum -= correlation_sum**2
-    correlation_squared_sum = torch.sqrt(torch.clamp(correlation_squared_sum, min=0))
+    corr_mean, corr_variance = correlation_sum_and_squared_sum_to_mean_and_variance(
+        correlation_sum, correlation_squared_sum, total_correlation_positions
+    )
 
     # Calculate normalized MIP
-    mip_scaled = mip - correlation_sum
+    mip_scaled = mip - corr_mean
     torch.where(
-        correlation_squared_sum != 0,  # preventing zero division error, albeit unlikely
-        mip_scaled / correlation_squared_sum,
+        corr_variance != 0,  # preventing zero division error, albeit unlikely
+        mip_scaled / corr_variance,
         torch.zeros_like(mip_scaled),
         out=mip_scaled,
     )
 
-    mip = mip * (num_pixels**0.5)
+    # Update correlation_sum and correlation_squared_sum to mean and variance
+    correlation_sum = corr_mean
+    correlation_squared_sum = corr_variance
 
     return mip, mip_scaled

src/tt2dtm/utils/pre_processing.py
@@ -119,11 +119,6 @@ def calculate_ctf_filter_stack(
 def do_image_preprocessing(image: torch.Tensor) -> torch.Tensor:
     """Pre-processes the input image before running the algorithm.
 
-    NOTE: Although we want an image with mean zero and variance 1, we do not divide by
-    the number of pixels because the CCG normalization requires that the CCG be divided
-    by the number of elements. This operation is skipped to save computation time during
-    the search.
-
     1. RFFT
     2. Calculate whitening filter
     2. Zero central pixel
@@ -160,8 +155,7 @@ def do_image_preprocessing(image: torch.Tensor) -> torch.Tensor:
     squared_sum = squared_image_dft.sum() + squared_image_dft[:, 1:].sum()
     image_dft /= torch.sqrt(squared_sum)
 
-    # NOTE: skip this operation -- see docstring
-    # image_dft *= image.numel()  # Scale to variance 1 in real-space
+    image_dft *= image.numel()  # Scale to variance 1 in real-space
 
     return image_dft
 